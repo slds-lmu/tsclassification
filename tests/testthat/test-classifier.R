302a29fc077f8ed3b7bef42a434b21d6aa8cd33c
pfistfl <pfistererf@googlemail.com>
2018-12-16 12:28:01 +0100

    Update tests

--- tests/testthat/test-classifier.R
+++ tests/testthat/test-classifier.R
@@ -1,30 +1,35 @@
 context("test-classifier")
 
 test_that("TSC can be built from file path", {
-  train_data = "inst/arff/GunPoint_TRAIN.arff"
+  train_data = file.path(system.file("arff", package="tsclassification"), "GunPoint_TRAIN")
   classifName = "weka.classifiers.meta.RotationForest"
   tsc = TSClassifier$new(train_data, classifName)
+  expect_class(tsc, classes = c("TSClassifier", "R6"))
 })
 
 test_that("TSC can be trained from file path", {
-  train_data = "inst/arff/GunPoint_TRAIN.arff"
+  train_data = file.path(system.file("arff", package="tsclassification"), "GunPoint_TRAIN")
   classifName = "weka.classifiers.meta.RotationForest"
   tsc = TSClassifier$new(train_data, classifName)
   tsc$train()
+  expect_class(tsc, classes = c("TSClassifier", "R6"))
+  expect_true(tsc$trained)
 })
 
 test_that("TSC can be tested from file path", {
-  train_data = "inst/arff/GunPoint_TRAIN.arff"
-  test_data = "inst/arff/GunPoint_TEST.arff"
+  train_data = file.path(system.file("arff", package="tsclassification"), "GunPoint_TRAIN")
+  test_data  = file.path(system.file("arff", package="tsclassification"), "GunPoint_TEST")
   classifName = "weka.classifiers.meta.RotationForest"
   tsc = TSClassifier$new(train_data, classifName)
   tsc$train()
-  tsc$predict(test_data)
+  expect_true(tsc$trained)
+  p = tsc$predict(test_data)
+  expect_numeric(p)
 })
 
 test_that("Test different impls", {
-  train_data = "inst/arff/GunPoint_TRAIN.arff"
-  test_data = "inst/arff/GunPoint_TEST.arff"
+  train_data = file.path(system.file("arff", package="tsclassification"), "GunPoint_TRAIN")
+  test_data  = file.path(system.file("arff", package="tsclassification"), "GunPoint_TEST")
   classifName = "weka.classifiers.meta.OptimisedRotationForest"
   tsc = TSClassifier$new(train_data, classifName)
   tsc$train()
--- tests/testthat/test-data_to_path.R
+++ tests/testthat/test-data_to_path.R
@@ -4,6 +4,9 @@ test_that("writing / deleting arff works ", {
   df = data.frame(matrix(rnorm(100), nrow = 10))
   df$target = sample(c(1, 0), 10, replace = TRUE)
   fp = data_to_path(df)
+  expect_true(file.exists(fp))
+  df2 = farff::readARFF(fp)
+  expect_equal(dim(df), dim(df2))
   delete_data_from_path(fp)
   expect_true(!file.exists(fp))
 })
@@ -14,5 +17,5 @@ test_that("Training on data.frame's works ", {
   train_data = df
   classifName = "weka.classifiers.meta.RotationForest"
   tsc = TSClassifier$new(train_data, classifName)
-  tsc$train()
+  expect_silent(tsc$train())
 })
