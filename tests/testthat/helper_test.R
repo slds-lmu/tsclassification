8429fb0a77b8c3ef6a9283dda1e8b0135f9e220b
pfistfl <pfistererf@googlemail.com>
2019-07-19 16:51:36 +0200

    All unit tests now pass

--- tests/testthat/helper_test.R
+++ tests/testthat/helper_test.R
@@ -1,4 +1,4 @@
-test_classifier = function(classifName) {
+test_classifier = function(classifName, len = 150) {
   train_data = file.path(system.file("arff", package="tsclassification"), "GunPoint_TRAIN.arff")
   test_data  = file.path(system.file("arff", package="tsclassification"), "GunPoint_TEST.arff")
   tsc = TSClassifier$new(classifName)
@@ -11,24 +11,24 @@ test_classifier = function(classifName) {
 
   p = tsc$predict(test_data)
   expect_factor(p)
-  expect_true(length(p) == 150)
+  expect_true(length(p) == len)
   expect_output(print(tsc), "Trained", fixed = TRUE)
   invisible(TRUE)
 }
 
 
-train_test_on_dataset = function(classifName, train_data, test_data) {
+train_test_on_dataset = function(classifName, train_data, test_data, len = 150L) {
   tsc = TSClassifier$new(classifName)
   expect_output(print(tsc), "Untrained")
 
-  tsc$train(train_data)
+  capture.output({tsc$train(train_data)})
   expect_true(tsc$trained)
   expect_output(print(tsc), paste0("Classifier: ", classifName), fixed = TRUE)
   expect_output(print(tsc), "Trained", fixed = TRUE)
 
-  p = tsc$predict(test_data)
+  capture.output({p <- tsc$predict(test_data)})
   expect_factor(p)
-  expect_true(length(p) == 150)
+  expect_true(length(p) == len)
   expect_output(print(tsc), "Trained", fixed = TRUE)
   invisible(TRUE)
 }
@@ -37,7 +37,7 @@ train_on_dataset = function(classifName, train_data) {
   tsc = TSClassifier$new(classifName)
   expect_output(print(tsc), "Untrained")
 
-  tsc$train(train_data)
+  capture.output({tsc$train(train_data)})
   expect_true(tsc$trained)
   expect_output(print(tsc), paste0("Classifier: ", classifName), fixed = TRUE)
   expect_output(print(tsc), "Trained", fixed = TRUE)
--- tests/testthat/test_classifier.R
+++ tests/testthat/test_classifier.R
@@ -94,109 +94,108 @@ test_that("TSC works with factors", {
 })
 
 
-# test_that("TSC predict works without target", {
-#   set.seed(30)
-#   m = matrix(runif(400, -1, 1), nrow = 40, ncol = 10)
-#   y = factor(apply(m, 1, mean) > 0, labels = c("a", "b"))
-#   m[,1] = ifelse(y == "a", m[,1] + 2, m[,1] - 2)
-#   dftrain = data.frame(m[1:20, ], "class" = y[1:20])
-#   dftest = data.frame(m[21:40, ], "class" = y[21:40])
-#   classifName = "weka.classifiers.trees.J48"
-#   tsc = TSClassifier$new(classifName)
-#   expect_class(tsc, "TSClassifier")
-#   tsc$train(dftrain, target = "class")
-#   expect_true(tsc$trained)
-#   dftest$class = NA
-#   p = tsc$predict(dftest)
-#   expect_factor(p, len = 20L, levels = c("a", "b"))
+test_that("TSC predict works without target", {
+  set.seed(30)
+  m = matrix(runif(400, -1, 1), nrow = 40, ncol = 10)
+  y = factor(apply(m, 1, mean) > 0, labels = c("a", "b"))
+  m[,1] = ifelse(y == "a", m[,1] + 2, m[,1] - 2)
+  dftrain = data.frame(m[1:20, ], "class" = y[1:20])
+  dftest = data.frame(m[21:40, ], "class" = y[21:40])
+  classifName = "weka.classifiers.trees.J48"
+  tsc = TSClassifier$new(classifName)
+  expect_class(tsc, "TSClassifier")
+  tsc$train(dftrain, target = "class")
+  expect_true(tsc$trained)
+  dftest$class = NA
+  p = tsc$predict(dftest)
+  expect_factor(p, len = 20L, levels = c("a", "b"))
 
-#   # Predict single instances
-#   p = tsc$predict(dftest[1,])
-#   expect_factor(p, len = 1L, levels = "a")
+  # Predict single instances
+  p = tsc$predict(dftest[1,])
+  expect_factor(p, len = 1L, levels = "a")
 
-#   # Predict single instances
-#   p = tsc$predict(dftest[2,])
-#   expect_factor(p, len = 1L, levels = "b")
-# })
+  # Predict single instances
+  p = tsc$predict(dftest[2,])
+  expect_factor(p, len = 1L, levels = "b")
+})
 
-# test_that("TSC predict works with multiclass", {
-#   set.seed(30)
-#   m = matrix(runif(400, -1, 1), nrow = 80, ncol = 5)
-#   y = factor(sample(c("a", "b", "c"), 80, replace = TRUE))
-#   m[,1] = ifelse(y == "a", m[,1] + 2, m[,1] - 2)
-#   m[,2] = ifelse(y == "b", m[,2] + 2, m[,2] - 2)
-#   m[,3] = ifelse(y == "c", m[,3] + 2, m[,3] - 2)
-#   m[,4] = ifelse(y == "a", m[,4] + 2, m[,4] - 2)
-#   m[,5] = ifelse(y == "c", m[,5] + 2, m[,5] - 2)
-#   dftrain = data.frame(m[1:40, ], "class" = y[1:40])
-#   dftest = data.frame(m[41:80, ], "class" = y[41:80])
+test_that("TSC predict works in several arcane cases", {
+  set.seed(30)
+  m = matrix(runif(400, -1, 1), nrow = 80, ncol = 5)
+  y = factor(sample(c("a", "b", "c"), 80, replace = TRUE))
+  m[,1] = ifelse(y == "a", m[,1] + 2, m[,1] - 2)
+  m[,2] = ifelse(y == "b", m[,2] + 2, m[,2] - 2)
+  m[,3] = ifelse(y == "c", m[,3] + 2, m[,3] - 2)
+  m[,4] = ifelse(y == "a", m[,4] + 2, m[,4] - 2)
+  m[,5] = ifelse(y == "c", m[,5] + 2, m[,5] - 2)
+  dftrain = data.frame(m[1:40, ], "class" = y[1:40])
+  dftest = data.frame(m[41:80, ], "class" = y[41:80])
 
-#   # Binary: Should work
-#   file_train = tempfile(pattern = "train", fileext = ".arff")
-#   file_test = tempfile(pattern = "test", fileext = ".arff")
-#   farff::writeARFF(dftrain[dftrain$class != "a", ], file_train, overwrite = TRUE)
-#   farff::writeARFF(dftest[dftest$class != "a", ], file_train, overwrite = TRUE)
-#   train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test)
+  # Binary: Should work
+  file_train = tempfile(pattern = "train", fileext = ".arff")
+  file_test = tempfile(pattern = "test", fileext = ".arff")
+  farff::writeARFF(dftrain[dftrain$class != "a", ], file_train, overwrite = TRUE)
+  farff::writeARFF(dftest[dftest$class != "a", ], file_test, overwrite = TRUE)
+  train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test, len = nrow(dftest[dftest$class != "a", ]))
 
-#   # No target in test: Should work
-#   file_train = tempfile(pattern = "train", fileext = ".arff")
-#   file_test = tempfile(pattern = "test", fileext = ".arff")
-#   farff::writeARFF(dftrain[dftrain$class != "a", ], file_train, overwrite = TRUE)
-#   dftest_nocl = dftest[dftest$class != "a", ]
-#   dftest_nocl$class = NULL
-#   farff::writeARFF(dftest_nocl, file_test, overwrite = TRUE)
-#   train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test)
+  # No target in test: Should work
+  file_train = tempfile(pattern = "train", fileext = ".arff")
+  file_test = tempfile(pattern = "test", fileext = ".arff")
+  farff::writeARFF(dftrain[dftrain$class != "a", ], file_train, overwrite = TRUE)
+  dftest_nocl = dftest[dftest$class != "a", ]
+  dftest_nocl$class = NULL
+  farff::writeARFF(dftest_nocl, file_test, overwrite = TRUE)
+  train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test, len = nrow(dftest_nocl))
 
-#   # NA target in test: Should work
-#   file_test = tempfile(pattern = "test", fileext = ".arff")
-#   dftest_nocl$class = NA
-#   farff::writeARFF(dftest_nocl, file_test, overwrite = TRUE)
-#   train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test)
+  # NA target in test: Should work
+  file_test = tempfile(pattern = "test", fileext = ".arff")
+  dftest_nocl$class = NA
+  farff::writeARFF(dftest_nocl, file_test, overwrite = TRUE)
+  train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test, len = nrow(dftest_nocl))
 
-#   # Multiclass
-#   file_train = tempfile(pattern = "train", fileext = ".arff")
-#   file_test = tempfile(pattern = "test", fileext = ".arff")
-#   farff::writeARFF(dftrain, file_train, overwrite = TRUE)
-#   farff::writeARFF(dftest, file_test, overwrite = TRUE)
-#   train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test)
+  # Multiclass
+  file_train = tempfile(pattern = "train", fileext = ".arff")
+  file_test = tempfile(pattern = "test", fileext = ".arff")
+  farff::writeARFF(dftrain, file_train, overwrite = TRUE)
+  farff::writeARFF(dftest, file_test, overwrite = TRUE)
+  train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test, len = nrow(dftest))
 
-#   # Full NA Data during train: This should error / reduce to majorityclass
-#   dftrain_na = dftrain
-#   dftrain_na[1:10, 1:5] = NA
-#   file_train = tempfile(pattern = "train", fileext = ".arff")
-#   farff::writeARFF(dftrain_na[1:10,], file_train, overwrite = TRUE)
-#   train_on_dataset("weka.classifiers.trees.J48", file_train)
+  # Full NA Data during train: This should error / reduce to majorityclass
+  dftrain_na = dftrain
+  dftrain_na[1:10, 1:5] = NA
+  file_train = tempfile(pattern = "train", fileext = ".arff")
+  farff::writeARFF(dftrain_na[1:10,], file_train, overwrite = TRUE)
+  expect_error(train_on_dataset("weka.classifiers.trees.J48", file_train), class = "IOException")
 
-#   # NA labels during train: This should error!
-#   dftrain_na = dftrain
-#   dftrain_na[, 6] = NA
-#   file_train = tempfile(pattern = "train", fileext = ".arff")
-#   farff::writeARFF(dftrain_na, file_train, overwrite = TRUE)
-#   train_on_dataset("weka.classifiers.trees.J48", file_train)
+  # NA labels during train: This should error!
+  dftrain_na = dftrain
+  dftrain_na[, 6] = NA
+  file_train = tempfile(pattern = "train", fileext = ".arff")
+  farff::writeARFF(dftrain_na, file_train, overwrite = TRUE)
+  expect_error(train_on_dataset("weka.classifiers.trees.J48", file_train), class = "IOException")
 
-#   # Partial NA Data during train: At least a warning
-#   dftrain_na2 = dftrain[dftrain$class != "a", ]
-#   dftrain_na2[1:10, 1:5] = NA
-#   file_train = tempfile(pattern = "train", fileext = ".arff")
-#   farff::writeARFF(dftrain_na[1:20,], file_train, overwrite = TRUE)
-#   train_on_dataset("weka.classifiers.trees.J48", file_train)
+  # Partial NA Data during train: At least a warning
+  dftrain_na2 = dftrain[dftrain$class != "a", ]
+  dftrain_na2[1:10, 1:5] = NA
+  file_train = tempfile(pattern = "train", fileext = ".arff")
+  farff::writeARFF(dftrain_na[1:20,], file_train, overwrite = TRUE)
+  expect_error(train_on_dataset("weka.classifiers.trees.J48", file_train), class = "IOException")
 
-#   # Full NA during test: This should either error or predict majority class
-#   file_train = tempfile(pattern = "train", fileext = ".arff")
-#   farff::writeARFF(dftrain[dftrain$class != "a", ], file_train, overwrite = TRUE)
+  # Full NA during test: This should error
+  file_train = tempfile(pattern = "train", fileext = ".arff")
+  farff::writeARFF(dftrain[dftrain$class != "a", ], file_train, overwrite = TRUE)
+  file_test = tempfile(pattern = "test", fileext = ".arff")
+  dftest_na = dftest
+  dftest_na[1:10, 1:5] = NA
+  farff::writeARFF(dftest_na[1:10,], file_test, overwrite = TRUE)
+  expect_error(train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test, len = 10), class = "IOException")
 
-#   file_test = tempfile(pattern = "test", fileext = ".arff")
-#   dftest_na = dftest
-#   dftest_na[1:10, 1:5] = NA
-#   farff::writeARFF(dftrain_na[1:10,], file_test, overwrite = TRUE)
-#   train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test)
-
-#   # NA Data during test: At least warn or majority class
-#   file_test = tempfile(pattern = "test", fileext = ".arff")
-#   dftest_na = dftest
-#   dftest_na[1:10, 1:5] = NA
-#   farff::writeARFF(dftest_na, file_test, overwrite = TRUE)
-#   train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test)
-# })
+  # NA Data during test: At least warn or majority class
+  file_test = tempfile(pattern = "test", fileext = ".arff")
+  dftest_na = dftest
+  dftest_na[1:10, 1:5] = NA
+  farff::writeARFF(dftest_na, file_test, overwrite = TRUE)
+  expect_error(train_test_on_dataset("weka.classifiers.trees.J48", file_train, file_test), class = "IOException")
+})
 
 
